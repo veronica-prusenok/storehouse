d2c6b897f43511db44828f40351fd3c3546f69e6
Polomki <happy.dariush@gmail.com>
2017-03-27 16:36:31 +0300

    Protect admins resources.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < ActionController::Base
   def after_invite_path_for(inviter, invitee)
     users_manage_index_path
   end
+
+  def authenticate_inviter!
+    authenticate_user!(:force => true)
+    authorize_admin!
+  end
+
+  private
+
+  def authorize_admin!
+    redirect_to root_path unless current_user.admin?
+  end
+
 end
--- app/controllers/users/manage_controller.rb
+++ app/controllers/users/manage_controller.rb
@@ -1,7 +1,10 @@
 module Users
   class ManageController < ApplicationController
+    before_action :authorize_admin!
+
     def index
       @resources = User.not_admins
     end
+
   end
 end
