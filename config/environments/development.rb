51ecf34d427d52078dc7a8581ebb1dffb1112126
Polomki <happy.dariush@gmail.com>
2017-03-27 14:17:38 +0300

    Change configs for mailer.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,12 +25,9 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
   
+  config.action_mailer.perform_caching = false
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
@@ -47,6 +44,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+
+  config.action_mailer.raise_delivery_errors = false
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
