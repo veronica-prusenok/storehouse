7e761336e9fccbce36d4a3dfc66cb2bca5758ca3
Polomki <happy.dariush@gmail.com>
2017-03-27 18:07:50 +0300

    Add some products methods.

--- app/controllers/products/manage_controller.rb
+++ app/controllers/products/manage_controller.rb
@@ -1,7 +1,42 @@
 module Products
   class ManageController < ApplicationController
+    before_action :authorize_admin!
+
     def index
       @products = Product.all
+    end
+
+    def new
+      @product = Product.new
+    end
+
+    def edit
+      @product = Product.find(params[:id])
+    end
+
+    def create
+      @product = Product.new(product_params)
+      @product.count = 0
+      if @product.save
+        flash[:notice] = t('products.created')
+        redirect_to action: :index
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @product.update(product_params)
+        flash[:notice] = t('products.updted')
+        redirect_to action: :index
+      else
+        render :edit
+      end
+    end
+
+    private
+    def product_params
+      params.require(:product).permit(:title, :image)
     end
   end
 end
