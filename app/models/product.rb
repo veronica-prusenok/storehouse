396d6903112ac870d1003bab2dc454d12ae81241
Polomki <happy.dariush@gmail.com>
2017-03-31 10:33:17 +0300

    Add some associations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :order_items
+  has_many :orders, through: :order_items
+
   validates :title, uniqueness: true, presence: true
 
   has_attached_file :image, styles: { medium: "150x150>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
