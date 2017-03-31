726fa408fc2e1c6a0616918afcb2433af3d29b71
Polomki <happy.dariush@gmail.com>
2017-03-31 10:08:50 +0300

    Fix mails bug.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => ENV["HOST"], :port => ENV["SMTP_PORT"] }
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
